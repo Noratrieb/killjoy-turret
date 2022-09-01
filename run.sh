950b1cce13d5fa5439721b6b5ac5d481412efae6
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2022-09-01 21:45:02 +0200

    run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -d -v (pwd)/config.json:/app/config.json -e CONFIG_PATH=/app/config.json killjoy-turret:1.0
+docker run -d -v (pwd)/config.json:/app/config.json -e CONFIG_PATH=/app/config.json --name killjoy-turret killjoy-turret:1.0
